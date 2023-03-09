084b529a898e389ded0d0d6a2f7b175c54a3f6be
omkar.shetkar <omkar.shetkar@hcl.com>
2023-03-09 10:38:47 +0530

    Node template added

---
+++ key-value-store/src/main/java/com/example/server/Node.java
@@ -0,0 +1,22 @@
+package com.example.server;
+
+import java.util.Map;
+
+public class Node {
+
+    private String name;
+    private Map<String, String> data;
+
+    public Node(String name, Map<String, String> data) {
+        this.name = name;
+        this.data = data;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, String> getData() {
+        return data;
+    }
+}
