7b909238f5c96851e112c44b878d86db078c1d8f
Omkar Shetkar <omkar.shetkar@outlook.com>
2023-07-03 06:03:47 +0530

    URL Shortener - get short url logic

--- url-shortener/src/main/java/com/example/UrlShortenerController.java
+++ url-shortener/src/main/java/com/example/UrlShortenerController.java
@@ -1,7 +1,28 @@
 package com.example;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class UrlShortenerController {
+
+    private final UrlShortenerService urlShortenerService;
+
+    public UrlShortenerController(UrlShortenerService urlShortenerService) {
+        this.urlShortenerService = urlShortenerService;
+    }
+
+    @PostMapping("/shorten")
+    public ResponseEntity<String> shortUrl(@RequestBody String longUrl) {
+
+        // Logic to generate a short URL
+        String shortUrl = urlShortenerService.generateShortUrl(longUrl);
+
+        // Store the mapping between short URL and long URL
+
+        return new ResponseEntity<>(shortUrl, HttpStatus.CREATED);
+    }
+
 }
