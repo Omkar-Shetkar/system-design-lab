e7c67091d688fe86ad5044470e06807adfca7133
Omkar Shetkar <omkar.shetkar@outlook.com>
2023-07-05 05:38:02 +0530

    URL Shortener - get short url logic

--- url-shortener/src/main/java/com/example/ShortUrlEntity.java
+++ url-shortener/src/main/java/com/example/ShortUrlEntity.java
@@ -1,8 +1,17 @@
 package com.example;
 
-//import jakarta.persistence.Entity;
-//
-//@Entity
+import jakarta.persistence.Entity;
+import org.springframework.data.annotation.Id;
+
+@Entity
 public class ShortUrlEntity {
 
+    @Id
+    private Long id;
+
+    private String shortUrl;
+
+    private String longUrl;
+
+    public ShortUrlEntity() {}
 }
