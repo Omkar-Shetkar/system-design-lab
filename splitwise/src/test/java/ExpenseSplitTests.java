5afa2b8adb184961c5423ac4d1d8a3bba1a4de7e
Omkar Shetkar <omkar.shetkar@outlook.com>
2024-12-01 17:47:41 +0530

    doc update

--- splitwise/src/test/java/ExpenseSplitTests.java
+++ splitwise/src/test/java/ExpenseSplitTests.java
@@ -40,20 +40,5 @@ public class ExpenseSplitTests {
 
     }
 
-    @Test
-    public void expenseTest1() {
-        SplitDetails splitDetails = new SplitDetails("EXPENSE u4 1200 4 u1 u2 u3 u4 PERCENT 40 20 20 20");
-        User creditor = splitDetails.creditor();
-        creditor.pay(splitDetails);
-
-        System.out.println("EXPENSE u4 1200 4 u1 u2 u3 u4 PERCENT 40 20 20 20");
-        creditor.show();
-        System.out.println("-------------------------------");
-        for(User borrower: splitDetails.borrowers()) {
-            borrower.show();
-        }
-        System.out.println("-------------------------------");
-
-    }
 
 }
